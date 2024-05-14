5011f4c0319de29503f0c78b17ef76476d9ce4d4
Will <doggy.huang@gmail.com>
2024-05-15 00:47:23 +0800

    改用 hanzidentifier 來計算中文數量

--- main.py
+++ main.py
@@ -10,27 +10,32 @@ import os
 import sys
 import codecs
 import tiktoken
-import langdetect
+import hanzidentifier
 
 sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
 
 ENCODING_NAME = "o200k_base"
+DETECT_METHOD = "hanzidentifier"
 
 tokenizer = tiktoken.get_encoding(ENCODING_NAME)
 
-if not os.path.exists(ENCODING_NAME):
-    os.makedirs(ENCODING_NAME)
+if not os.path.exists(f"{ENCODING_NAME}-{DETECT_METHOD}"):
+    os.makedirs(f"{ENCODING_NAME}-{DETECT_METHOD}")
 
 for i in range(tokenizer.eot_token - 1):
     term: str = tokenizer.decode([i])
-    lang: str = "unknown"
+    lang: str = "others"
 
-    try:
-        lang = langdetect.detect(term)
-    except langdetect.lang_detect_exception.LangDetectException:
-        pass
+    if hanzidentifier.has_chinese(term):
+        lang = "zh"
+
+        if hanzidentifier.is_simplified(term):
+            lang = "zh-cn"
+
+        if hanzidentifier.is_traditional(term):
+            lang = "zh-tw"
 
     print(f"{i:06d} {lang} {term}")
 
-    with open(f"{ENCODING_NAME}/{lang}.txt", "a", encoding="utf-8") as file:
+    with open(f"{ENCODING_NAME}-{DETECT_METHOD}/{lang}.txt", "a", encoding="utf-8") as file:
         file.write(f"{i:06d} {term}\n")
